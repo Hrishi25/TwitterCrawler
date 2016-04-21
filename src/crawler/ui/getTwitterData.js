95a3e4d9e53fe052c94e6383328a93d86498fa2f
krcox <krcox@10.63.61.166>
2016-04-21 13:34:07 -0700

    Accessing table with twitter data

--- src/crawler/ui/getTwitterData.js
+++ src/crawler/ui/getTwitterData.js
@@ -3,23 +3,37 @@
  */
 function coolMessage() {
 	alert("Cool message");
-}
+};
 
 window.onload=function() {
-	coolMessage();
-}
+	scanTable();
+};
 
 /**
  * AWS Configuration
  */
-var AWS.config = new AWS.Config({
+var dynamodb = new AWS.DynamoDB({
+	params: {TableName: 'twitter_data'},
+	endpoint: 'http://dynamodb.us-west-2.amazonaws.com',
 	accessKeyId: 'AKIAJ5BSIE7EMETRCAKQ', 
-	secretAccessKey: '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', 
+	secretAccessKey: 'o9Z1mmudBQoY25ulKFBii/wgHvTnClHSSi811/XN',
 	region: 'us-west-2'
 });
 
-var table = new AWS.DynamoDB({
-	params: {TableName: 'Twitter'},
-	endpoint: 
+/**
+ * Scan DynamoDB Table for items
+ */
+var params = {
+		TableName: 'twitter_data', /* required */
+		Select: 'ALL_ATTRIBUTES'
+};
 
-});
+function scanTable() {
+	alert("scanning table");
+	var twitterItems = dynamodb.scan(params, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else     console.log(data);           // successful response
+	});
+	console.log(twitterItems);
+};
+
