0ce7537297c5c17188f5347ae36b6fe7a8b009e1
krcox <krcox@10.63.60.176>
2016-04-21 12:46:15 -0700

    Adding AWS Credentials

--- src/crawler/ui/getTwitterData.js
+++ src/crawler/ui/getTwitterData.js
@@ -2,18 +2,24 @@
  * Display a cool message.
  */
 function coolMessage() {
-    alert("Cool message");
+	alert("Cool message");
 }
 
 window.onload=function() {
 	coolMessage();
 }
 
-   /**
-   * AWS Configuration
-   */
-    var AWS.config = new AWS.Config({
-	  accessKeyId: 'AKIAJ5BSIE7EMETRCAKQ', 
-	  secretAccessKey: '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', 
-	  region: 'us-west-2'
-	});
+/**
+ * AWS Configuration
+ */
+var AWS.config = new AWS.Config({
+	accessKeyId: 'AKIAJ5BSIE7EMETRCAKQ', 
+	secretAccessKey: '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', 
+	region: 'us-west-2'
+});
+
+var table = new AWS.DynamoDB({
+	params: {TableName: 'Twitter'},
+	endpoint: 
+
+});
